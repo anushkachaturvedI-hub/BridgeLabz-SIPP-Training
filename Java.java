f4a7fc0666e4ae2648c887c845f9a8634bd3f6fc
ANUSHKA <anushka.chaturvedi_cs24@gla.ac.in>
2025-08-11 15:21:12 +0530

    second

--- Java.java
+++ Java.java
@@ -1,5 +1,9 @@
 public class Java {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello, World!, Anushka");
+=======
+        System.out.println("Hello, World!, Adwitiya");
+>>>>>>> Stashed changes
     }
 }
